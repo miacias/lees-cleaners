78ec828830c8b3d829d9712ec4e73fc06b598ae2
miacias <miaciasullo@gmail.com>
2024-03-16 16:59:30 -0400

    client suggested design contains flaws and cannot be sent to production. pinned sidebar is too tall and will break on vertically shortened viewports and horizontally shortened viewports. completely inaccessible on mobile. pinned header not viable since there is so little content to scroll through on page. no footer suggested. consider instead unpinning sidebar and header, moving contact info (hours, address, phone) to footer and adding map embed. consider moving Services to <main> and adding descriptive text. consider moving decorative images and blurbs to be interspersed with Services main content. lastly, consider keeping a list of services under header that is clickable and scrolls directly to content section.

--- src/components/Workspace/Workspace.jsx
+++ src/components/Workspace/Workspace.jsx
@@ -5,8 +5,7 @@ const Workspace = () => {
   return (
     <div className={`workspace ${styles.workspace}`}>
       <ul className={`info ${styles.info}`}>
-        <li>Measurement appointments available.</li>
-        <li>Walk-ins welcome!</li>
+        <li>All alterations done on site</li>
       </ul>
       <img src={workspace} alt="tailor workspace" style={{width: '400px'}}/>
     </div>
